ae5e753119848faa5789a3f54c59250bb68b1016
SaiChandana02 <85987487+SaiChandana02@users.noreply.github.com>
2022-07-21 19:47:20 +0530

    Update student.py

---
+++ student.py
@@ -0,0 +1,46 @@
+
+from user import User
+
+class Student(User)):
+
+    allStudents = []
+
+    def init(self, Rollno):
+        self.rollno = Rollno
+        self.myBooks = []
+        
+
+    def requestBookName():
+        pass
+
+    def returnBook():
+        pass
+
+    def findMyBookByName():
+        pass
+
+
+    @staticmethod
+    def addStudent():
+        pass
+
+    @staticmethod
+    def readStudent():
+        pass
+
+    @staticmethod
+    def updateStudent():
+        pass
+
+    @staticmethod
+    def deleteStudent():
+        pass
+
+
+
+
+
+
+
+
+        
